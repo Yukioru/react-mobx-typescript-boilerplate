02eb2f9be022860cb567aa108ea786cfb4101e8b
Yukioru <obt195@gmail.com>
2017-02-15 21:46:49 +0400

    fix types

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,17 +1,7 @@
-/** Global definitions for developement **/
-
-// for style loader
 declare module '*.css' {
   const styles: any;
   export = styles;
 }
 
-// for commonjs modules
-// NOTE: remove below if you install types for node. (for server developement)
-interface RequireInterface {
-  (module: string): any;
-  ensure?(module: string): any;
-}
-declare var module: any;
-declare var process: any;
-declare var require: RequireInterface;
+declare var module: NodeModule;
+declare var require: NodeRequire;
